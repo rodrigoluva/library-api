f2bdbea4287126ae96e95adf9b97adbfd8ab0e1d
Rodrigo Valladão <rodrigoluva@gmail.com>
2026-07-08 11:22:44 -0300

    feat: add Book and BookCopy model

--- library_api/models/books.py
+++ library_api/models/books.py
@@ -1,8 +1,14 @@
 from datetime import date, datetime
+from enum import Enum
 from typing import Optional
 from sqlalchemy import ForeignKey, String, Text, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from library_api.models import Base
+
+
+class BookStatus(str, Enum):
+    AVAILABLE = 'available'
+    BORROWED = 'borrowed'
 
 
 class Author(Base):
@@ -18,4 +24,50 @@ class Author(Base):
     updated_at: Mapped[datetime] = mapped_column(
         onupdate=func.now(),
         server_default=func.now(),
+    )
+
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    isbn: Mapped[str] = mapped_column(unique=True, index=True)
+    published_date: Mapped[Optional[date]]
+    author_id: Mapped[int] = mapped_column(
+        ForeignKey('authors.id'),
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        onupdate=func.now(),
+        server_default=func.now(),
+    )
+
+    author: Mapped['Author'] = relationship(
+        'Author',
+        back_populates='authors'
+    )
+
+
+class BookCopy(Base):
+    __tablename__ = 'book_copies'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    book_id: Mapped[int] = mapped_column(
+        ForeignKey('books.id')
+    )
+    status: Mapped[BookStatus] = mapped_column(String(15))
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        onupdate=func.now(),
+        server_default=func.now(),
+    )
+
+    book: Mapped['Book'] = relationship(
+        'Book',
+        back_populates='book_copies'
     )
